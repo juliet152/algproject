1dbf729b0d44609c54e0203b8b5f4b6fd242aab8
juliet152 <juliet_152@naver.com>
2016-12-03 00:08:55 +0900

    i hate github

--- algproject/main.cpp
+++ algproject/main.cpp
@@ -1,6 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
+	int n;
+	printf("Hello, world!\n");
+	printf("I hate git-hub\n");
+	scanf("%d",&n);
 	return 0;
 }
